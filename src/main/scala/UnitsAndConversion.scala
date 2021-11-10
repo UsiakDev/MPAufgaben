8e5ee3fed0c7688c37e5e012438426d46dc80f8f
Usiak <kevin.usiak@edu.fhdw.de>
2021-11-10 14:48:11 +0100

    UnitAndConversion Fix

--- src/main/scala/UnitsAndConversion.scala
+++ src/main/scala/UnitsAndConversion.scala
@@ -1,3 +1,5 @@
 object UnitsAndConversion {
-
+  abstract class ConversionMethod
+  case class ProportionalConversion(factor:Int)
+  case class LinearConversion(m:Int,b:Int)
 }
--- src/test/scala/UnitsAndConversionTests.scala
+++ src/test/scala/UnitsAndConversionTests.scala
@@ -1,8 +1,6 @@
 import org.scalatest.FunSuite
 
 class UnitsAndConversionTests extends FunSuite{
-  abstract class ConversionMethod
-  case class ProportionalConversion(factor:Int)
-  case class LinearConversion(m:Int,b:Int)
+
 
 }
