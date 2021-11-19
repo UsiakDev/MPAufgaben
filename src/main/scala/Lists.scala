2c0143742ba3ab173ca0b7e6eaa9142193020b83
Usiak <kevin.usiak@edu.fhdw.de>
2021-11-19 12:46:13 +0100

    List Update

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -1,5 +1,7 @@
 import MPAufgaben._
 
+import scala.annotation.tailrec
+
 object Lists {
 
   def main(args: Array[String]): Unit = {
@@ -38,4 +40,52 @@ object Lists {
     case _:emptyList => ""
   }
 
+  /**
+   * Makes a new list with all found Primes in given Int List
+   * @param x given List full of integers
+   * @return Outputs List with all found Prims
+   */
+  def getPrimes(x:List):List = x match{
+    case l:nonEmptyList => l.content match{
+      case content:Int =>
+        if(isPrim(content)) nonEmptyList(content, getPrimes(l.furtherList))
+        else getPrimes(l.furtherList)
+      case _ => throw new Exception("List is not made out of integers only")
+    }
+    case _:emptyList => emptyList()
+  }
+
+  /**
+   * Reverses a given List
+   * @param x given List
+   * @return Outputs reversed List
+   */
+  def reverseList(x:List):List = {
+    reverseListHelper(x)
+  }
+
+  //Helper Function of ReverseList
+  @tailrec private def reverseListHelper(l:List, save:List=emptyList()):List = l match{
+    case x:nonEmptyList => reverseListHelper(x.furtherList,nonEmptyList(x.content,save))
+    case _:emptyList => save
+  }
+
+  /**
+   * Appends 2 lists together
+   * @param x first given list
+   * @param y second given list
+   * @return Outputs a list with first list + second list appends
+   */
+  def append(x:List,y:List):List = x match{
+    case l:nonEmptyList => nonEmptyList(l.content,append(l.furtherList,y))
+    case _:emptyList => y
+  }
+
+  def flatten(x:List):List = x match{
+    case l:nonEmptyList => l.content match{
+      case list:nonEmptyList => ??? //nonEmptyList(list.content,append(flatten(list.furtherList),flatten(l.furtherList)))
+      case content:Any => ??? //nonEmptyList(content,flatten(l.furtherList))
+    }
+    case _:emptyList => emptyList()
+  }
 }
--- src/test/scala/ListsTest.scala
+++ src/test/scala/ListsTest.scala
@@ -3,6 +3,11 @@ import Lists._
 
 class ListsTest extends FunSuite{
   val randomList:List = nonEmptyList(2,nonEmptyList("Hallo",nonEmptyList(true,emptyList())))
+  val primList:List
+  = nonEmptyList(1,nonEmptyList(2,nonEmptyList(3,nonEmptyList(5,nonEmptyList(8,emptyList())))))
+  val notFlattendList:List
+  = nonEmptyList(nonEmptyList(2,nonEmptyList(true,emptyList())),nonEmptyList("Hallo",emptyList()))
+  val flattendList:List = nonEmptyList(2,nonEmptyList(true,nonEmptyList("Hallo",emptyList())))
 
   test("Length Test"){
     assert(lengthOfList(randomList)==3)
@@ -14,5 +19,70 @@ class ListsTest extends FunSuite{
     assert(toString2(emptyList())=="")
   }
 
+  test("getPrimesList Test"){
+    assert(getPrimes(primList)==nonEmptyList(2,nonEmptyList(3,nonEmptyList(5,emptyList()))))
+    assert(getPrimes(emptyList())==emptyList())
+  }
 
+  test("reverse List Test"){
+    assert(reverseList(randomList)==nonEmptyList(true,nonEmptyList("Hallo",nonEmptyList(2,emptyList()))))
+    assert(reverseList(emptyList())==emptyList())
+  }
+
+  test("append Test"){
+    assert(append(nonEmptyList(2,emptyList()),nonEmptyList(true,emptyList()))
+      ==nonEmptyList(2,nonEmptyList(true,emptyList())))
+
+    assert(append(emptyList(),nonEmptyList(true,emptyList()))
+      ==nonEmptyList(true,emptyList()))
+
+    assert(append(nonEmptyList(2,emptyList()),emptyList())
+      ==nonEmptyList(2,emptyList()))
+  }
+
+  test("flatten Test"){
+    assert(flatten(randomList)==randomList)
+    assert(flatten(notFlattendList)==flattendList)
+  }
+
+  /*
+  test("flattenTest") {
+
+    assert(flatten(emptyList()) == emptyList())
+
+    assert(flatten(nonEmptyList(1, emptyList())) == nonEmptyList(1, emptyList()))
+
+    assert(flatten(nonEmptyList(
+      nonEmptyList(
+        nonEmptyList(1,
+          nonEmptyList(2,
+            nonEmptyList(3, emptyList()))),
+        nonEmptyList(4,
+          nonEmptyList(5, emptyList()))),
+      nonEmptyList(6,
+        nonEmptyList(7,
+          nonEmptyList(8,
+            nonEmptyList(9, emptyList()))))))
+      == nonEmptyList(1,
+      nonEmptyList(2,
+        nonEmptyList(3,
+          nonEmptyList(4,
+            nonEmptyList(5,
+              nonEmptyList(6,
+                nonEmptyList(7,
+                  nonEmptyList(8,
+                    nonEmptyList(9, emptyList()))))))))))
+
+    assert(flatten(nonEmptyList(
+      nonEmptyList(1, emptyList()),
+      nonEmptyList(3, emptyList()))) == nonEmptyList(1, nonEmptyList(3, emptyList())))
+
+    assert(flatten(
+      nonEmptyList(
+        nonEmptyList(
+          nonEmptyList(1, emptyList()),
+          nonEmptyList(4, emptyList())),
+        nonEmptyList(1, emptyList()))) == nonEmptyList(1, nonEmptyList(4, nonEmptyList(1, emptyList()))))
+  }
+   */
 }
